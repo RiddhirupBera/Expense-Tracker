9aa8e326cc8a0625f6b2fa4285e950d6155814e6
Riddhirup Bera <riddhirup@Riddhirups-MacBook-Air.local>
2026-01-17 11:16:30 +0530

    d2

--- expense-tracker/app/page.tsx
+++ expense-tracker/app/page.tsx
@@ -1,17 +1,25 @@
-import ExpenseForm from "@/app/ExpenseForm";
-import ExpenseList from "@/app/ExpenseList";
-import { redirect } from "next/navigation";
-import { checkLoggedIn } from "./actions/checkLoggedIn";
+// import ExpenseForm from "@/app/ExpenseForm";
+// import ExpenseList from "@/app/ExpenseList";
+// import { redirect } from "next/navigation";
+// import { checkLoggedIn } from "./actions/checkLoggedIn";
 
-export default async function ExpensePage() {
-  redirect("/register");
-  await checkLoggedIn();
-  //redirect("/register");
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Expense Tracker</h1>
-      <ExpenseForm />
-      <ExpenseList />
-    </main>
-  );
+// export default async function ExpensePage() {
+//   redirect("/register");
+//   await checkLoggedIn();
+//   //redirect("/register");
+//   return (
+//     <main style={{ padding: "2rem" }}>
+//       <h1>Expense Tracker</h1>
+//       <ExpenseForm />
+//       <ExpenseList />
+//     </main>
+//   );
+// }
+
+
+// app/page.tsx
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/login"); // or /register or /dashboard
 }

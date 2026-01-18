c460644023019f2edd9a304547077c2928b4fcf7
Riddhirup Bera <riddhirup@Riddhirups-MacBook-Air.local>
2026-01-18 11:16:59 +0530

    structure changes

--- expense-tracker/app/page.tsx
+++ expense-tracker/app/page.tsx
@@ -4,22 +4,16 @@ import { redirect } from "next/navigation";
 import { checkLoggedIn } from "./actions/checkLoggedIn";
 
 export default async function ExpensePage() {
-  //redirect("/register");
+  redirect("/register");
   //await checkLoggedIn();
   //redirect("/register");
   return (
     <main style={{ padding: "2rem" }}>
       <h1>Expense Tracker</h1>
-      {/* <ExpenseForm />
-      <ExpenseList /> */}
+      <ExpenseForm />
+      <ExpenseList />
     </main>
   );
 }
 
 
-// app/page.tsx
-// import { redirect } from "next/navigation";
-
-// export default function Home() {
-//   redirect("/login"); // or /register or /dashboard
-// }
